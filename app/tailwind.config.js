67b4b0f79b4a7edb1401cfb3fee5aa1108a6e4b1
same.js <40536011+same-js@users.noreply.github.com>
2023-01-28 16:27:23 +0900

    [Add] Setting for using Tailwind CSS

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+    ],
+    mode: 'jit',
+    purge: [
+      './src/**/*.{js,ts,jsx,tsx}'
+    ],   
   theme: {
     extend: {},
   },
